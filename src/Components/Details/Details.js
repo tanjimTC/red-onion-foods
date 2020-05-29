9556e346b25686fd5dd0aa4b7198af1c457f4543
tanjimTC <tanjimtc74@gmail.com>
2020-05-30 02:01:22 +0600

    cart bug fixed

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,53 +1,83 @@
 import React, { useEffect, useState } from "react";
-import './Details.css';
+import "./Details.css";
 import { food } from "../../data/food";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 const Details = (props) => {
-  const {foodID} = useParams() ;
+  const { foodID } = useParams();
   const [foodDescription, setFoodDescription] = useState([]);
   const [count, setCount] = useState(1);
+  const [successMsg, setSuccessMsg] = useState(false);
   useEffect(() => {
     // const foodID = props.match.params.id;
     const foodDetails = food.find((x) => x.id === parseInt(foodID));
     setFoodDescription(foodDetails);
   }, [foodID]);
 
-  const handleClick =()=>{
+  const handleClick = () => {
     foodDescription.quantity = count;
     props.handleCart(foodDescription);
+    setSuccessMsg(true);
+  };
+
+  if (successMsg) {
+    setTimeout(() => setSuccessMsg(false), 1500);
   }
 
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6  d-flex justify-content-center align-items-center ">
-          <div className='left p-5'>
-            <h2><b>{foodDescription.foodTitle}</b></h2>
+          <div className="left p-5">
+            <h2>
+              <b>{foodDescription.foodTitle}</b>
+            </h2>
             <p>{foodDescription.foodDescription}</p>
-            
+
             <div className="d-flex my-4">
-            <h3 >${foodDescription.price}</h3>
+              <h3>${foodDescription.price}</h3>
               <div className="cart ml-3 btn">
                 <button
                   className="btn"
-                  onClick={()=> {if(count > 1){
-                      setCount(count-1)
-                  }}}
+                  onClick={() => {
+                    if (count > 1) {
+                      setCount(count - 1);
+                    }
+                  }}
                 >
                   -
                 </button>
-                    <button className="btn">{count}</button>
-                <button
-                  className="btn"
-                  onClick ={()=>setCount(count+1) }
-                >
+                <button className="btn">{count}</button>
+                <button className="btn" onClick={() => setCount(count + 1)}>
                   +
                 </button>
-              </div> 
+              </div>
             </div>
-            <button onClick={handleClick} className="cart btn add mt-2">
-             <span><img src="https://cdn0.iconfinder.com/data/icons/shopping-icons-5/100/Cart-32.png" alt="icon"/></span> Add</button>
+            <div className="d-flex align-items-center">
+              <button onClick={handleClick} className="cart btn add mt-2">
+                <span>
+                  <img
+                    src="https://cdn0.iconfinder.com/data/icons/shopping-icons-5/100/Cart-32.png"
+                    alt="icon"
+                  />
+                </span>{" "}
+                Add
+              </button>
+              {successMsg && (
+                <p
+                  style={{ color: "#fff" }}
+                  className="ml-3 p-2 success-msg bg-success"
+                >
+                  <span>
+                    <img
+                      src="https://cdn0.iconfinder.com/data/icons/healthy-17/64/eat-eating-food-32.png"
+                      alt=""
+                    />
+                  </span>{" "}
+                  Items Added To Cart!
+                </p>
+              )}
+            </div>
           </div>
         </div>
         <div className="col-md-6 p-4">
@@ -59,7 +89,6 @@ const Details = (props) => {
             />
           )}
         </div>
-        
       </div>
     </div>
   );
